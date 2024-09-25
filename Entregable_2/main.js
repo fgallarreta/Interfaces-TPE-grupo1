f6768f69ee211b0e1ee9ab0388ff9331e482843f
Facundo Gallarreta <facundo.gallarreta1999@gmail.com>
2024-09-25 12:48:16 -0300

    agregado botones usuario

--- Entregable_2/main.js
+++ Entregable_2/main.js
@@ -4,3 +4,7 @@ function desplegarMenu() {
     let menu = document.getElementById('menu');
     menu.classList.toggle('open');
 }
+function desplegarOpcionesUsuario() {
+    let opciones = document.getElementById('opcionesUsuario');
+    opciones.classList.toggle('mostrarOpciones');
+}
