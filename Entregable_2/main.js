4253994b3f71e6b5d47ab996b4adc5f3a641fc1c
Sebastian Cisneros <sebacisnero03@gmail.com>
2024-10-02 19:10:23 -0300

    Pantalla de carga

--- Entregable_2/main.js
+++ Entregable_2/main.js
@@ -8,3 +8,25 @@ function desplegarOpcionesUsuario() {
     let opciones = document.getElementById('userOptions');
     opciones.classList.toggle('mostrarOpciones');
 }
+const progressText = document.getElementById('progress-text');
+const loaderContainer = document.getElementById('loader-container');
+const content = document.getElementById('content');
+
+let progress = 0;
+const totalDuration = 5000; // Duración total de 5 segundos
+const intervalDuration = 100; // Intervalo de actualización en milisegundos
+
+const interval = setInterval(() => {
+    progress += intervalDuration;
+    const percentage = Math.min(Math.floor((progress / totalDuration) * 100), 100);
+    progressText.textContent = percentage + '%';
+
+    // Cuando el progreso llega al 100%, se oculta el loader y se muestra el contenido
+    if (percentage === 100) {
+        clearInterval(interval);
+        setTimeout(() => {
+        loaderContainer.style.display = 'none'; // Ocultar loader
+        content.style.display = 'block';        // Mostrar contenido de la página
+    }, 1000);
+    }
+}, intervalDuration);
