fc865aead0ba88b62768d03a68ab7740286bf541
Facundo Gallarreta <facundo.gallarreta1999@gmail.com>
2024-10-02 21:27:37 -0300

    Arregle el loader

--- Entregable_2/main.js
+++ Entregable_2/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+//Uso de funciones
+cargarLoader();
+
 function desplegarMenu() {
     let menu = document.getElementById('menu');
     menu.classList.toggle('open');
@@ -8,25 +11,31 @@ function desplegarOpcionesUsuario() {
     let opciones = document.getElementById('userOptions');
     opciones.classList.toggle('mostrarOpciones');
 }
-const progressText = document.getElementById('progress-text');
-const loaderContainer = document.getElementById('loader-container');
-const content = document.getElementById('content');
 
-let progress = 0;
-const totalDuration = 5000; // Duración total de 5 segundos
-const intervalDuration = 100; // Intervalo de actualización en milisegundos
+function cargarLoader() {
+    let progreso = document.getElementById('progreso');
+    let contenedorLoader = document.getElementById('loader');
+    let paginaGral = document.getElementById('paginaGral');
 
-const interval = setInterval(() => {
-    progress += intervalDuration;
-    const percentage = Math.min(Math.floor((progress / totalDuration) * 100), 100);
-    progressText.textContent = percentage + '%';
+    let inicio = 0;
+    let duracion = 5000;
+    let intervalo = 100;
 
-    // Cuando el progreso llega al 100%, se oculta el loader y se muestra el contenido
-    if (percentage === 100) {
-        clearInterval(interval);
-        setTimeout(() => {
-        loaderContainer.style.display = 'none'; // Ocultar loader
-        content.style.display = 'block';        // Mostrar contenido de la página
-    }, 1000);
-    }
-}, intervalDuration);
+    let interval = setInterval(() => {
+        inicio += intervalo;
+        let porcentaje = Math.min(Math.floor((inicio / duracion) * 100), 100);
+        progreso.textContent = porcentaje + '%';
+
+        // Cuando el progreso llega al 100%, se oculta el loader y se muestra el contenido
+        if (porcentaje === 100) {
+            clearInterval(interval);
+            setTimeout(() => {
+                contenedorLoader.style.display = 'none'; // Ocultar loader
+                paginaGral.style.display = 'block';      // Mostrar contenido de la página
+            }, 1000);
+        }
+    }, intervalo);
+};
+
+
+
